2db0d2df0cc47fdd94f51f58a603315fa1965c44
ecitlm <ecitlm@163.com>
2017-11-09 22:39:33 +0800

    add api sign

--- PhalApi/PhalApi/Filter/SimpleMD5.php
+++ PhalApi/PhalApi/Filter/SimpleMD5.php
@@ -17,11 +17,13 @@
  * @author      dogstar <chanzonghuang@gmail.com> 2015-10-23
  */
 
-class PhalApi_Filter_SimpleMD5 implements PhalApi_Filter {
+class PhalApi_Filter_SimpleMD5 implements PhalApi_Filter
+{
 
     protected $signName;
 
-    public function __construct($signName = 'sign') {
+    public function __construct($signName = 'sign')
+    {
         $this->signName = $signName;
     }
 
@@ -29,25 +31,28 @@ class PhalApi_Filter_SimpleMD5 implements PhalApi_Filter {
      * 签名验证 去除了sign和方法名参数
      * @throws PhalApi_Exception_BadRequest
      */
-    public function check() {
+    public function check()
+    {
 
         $service = DI()->request->get('service');
-        $app=DI()->config->get('app');
+        $app = DI()->config->get('app');
         $apiFilterRules = $app['apiFilterRules'];//获取不需要签名的接口配置文件
-        if (!in_array($service,$apiFilterRules)) {
+        if (!in_array($service, $apiFilterRules)) {
             $allParams = DI()->request->getAll();//获取所有的参数
             $sign = isset($allParams[$this->signName]) ? $allParams[$this->signName] : '';
             $timestamp = isset($allParams['timestamp']) ? $allParams['timestamp'] : '';
+
             //判断接口请求时间问题
-            if(empty($timestamp)) {
+            if (empty($timestamp)) {
                 throw new PhalApi_Exception_BadRequest("缺少时间戳参数", 6);
             }
-            $times = time() * 1000;
+
             //一个接口请求能使用300秒有效期
-            if ($times - floatval($timestamp) >300000) {
+            $times = time() * 1000;
+            if ($times - floatval($timestamp) > 300000) {
                 throw new PhalApi_Exception_BadRequest("接口请求超时", 6);
             }
-
+            
             if (empty($sign)) {
                 throw new PhalApi_Exception_BadRequest("缺少必要sign参数", 6);
             }
@@ -65,7 +70,8 @@ class PhalApi_Filter_SimpleMD5 implements PhalApi_Filter {
         }
     }
 
-    protected function encryptAppKey($params) {
+    protected function encryptAppKey($params)
+    {
         ksort($params);
 
         $paramsStrExceptSign = '';
